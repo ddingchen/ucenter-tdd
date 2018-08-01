7d15e13b2a703f507683450c8ddc293160674c71
ddingchen <183488175@qq.com>
2018-08-01 19:57:30 +0800

    使用SDK重构

--- app/Http/Controllers/UCenterController.php
+++ app/Http/Controllers/UCenterController.php
@@ -3,25 +3,16 @@
 namespace App\Http\Controllers;
 
 use GuzzleHttp\Client;
+use Thinker\Facades\UCenter;
 
 class UCenterController extends Controller
 {
 
     public function callback()
     {
-        $response = app(Client::class)->post(config('ucenter.root') . '/api/oauth/accessToken', [
-            'form_params' => [
-                'client_id' => config('ucenter.client_id'),
-                'client_secret' => config('ucenter.client_secret'),
-                'grant_type' => 'authorization_code',
-                'redirect_uri' => config('ucenter.redirect_uri'),
-                'code' => request('code'),
-            ],
-        ])->getBody();
+        $token = UCenter::webAuth()->token(request('code'));
 
-        $token = json_decode($response, true)['data'];
-
-        session()->put('ucenter', $token);
+        session()->put('ucenter', (array) $token);
 
         return redirect('/');
     }
--- app/Http/Middleware/AuthenticateWithUCenter.php
+++ app/Http/Middleware/AuthenticateWithUCenter.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Thinker\Facades\UCenter;
 
 class AuthenticateWithUCenter
 {
@@ -16,15 +17,7 @@ class AuthenticateWithUCenter
     public function handle($request, Closure $next)
     {
         if (!session()->has('ucenter')) {
-
-            $url = sprintf(
-                '%s/oauth/authorize?client_id=%s&redirect_uri=%s&response_type=code',
-                config('ucenter.root'),
-                config('ucenter.client_id'),
-                config('ucenter.redirect_uri')
-            );
-
-            return redirect($url);
+            return UCenter::webAuth()->redirect();
         }
 
         return $next($request);
