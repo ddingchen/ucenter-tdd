d416cf8a1e41109e74e8099070ca116edd3b4bfb
ddingchen <183488175@qq.com>
2018-08-01 20:09:05 +0800

    使用SDK重构单元测试

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -10,6 +10,7 @@ use GuzzleHttp\Psr7\Response;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Thinker\Facades\UCenterApi;
 
 class LoginTest extends TestCase
 {
@@ -17,12 +18,7 @@ class LoginTest extends TestCase
     public function test_it_redirects_ucenter_if_a_user_accesses_in_without_login_status()
     {
         $this->get('/')
-            ->assertRedirect(sprintf(
-            '%s/oauth/authorize?client_id=%s&redirect_uri=%s&response_type=code',
-            config('ucenter.root'),
-            config('ucenter.client_id'),
-            config('ucenter.redirect_uri')
-        ));
+            ->assertRedirect(UCenterApi::urlOfAuthorizePage());
     }
 
     public function test_it_shouldnt_redirect_ucenter_if_a_user_accesses_in_with_login_status()
@@ -35,32 +31,17 @@ class LoginTest extends TestCase
 
     public function test_it_redirects_home_with_login_status_if_a_user_successfully_login()
     {
-        $this->mockUCenterResponse([
-            "code" => 0,
-            "message" => "获取access_token成功",
-            "data" => $fakeToken = [
+        UCenterApi::fake()
+            ->mockResponse('getAccessTokenByCode', 'ok', $fakeToken = [
                 "access_token" => "AynyRZKKskMBs4ONjOHUecgAyM2rqpvToo0QTXPA",
                 "token_type" => "Bearer",
                 "expires_in" => 7200,
                 "refresh_token" => "mcQNthVcEJpn09MObyxXerv4tiQq9I2z85NAe2ye"
-            ]
-        ]);       
+            ]);
 
         $this->get(config('ucenter.redirect_uri') . '?code=123')
             ->assertRedirect('/')
             ->assertSessionHas('ucenter', $fakeToken);
-    }
-
-    protected function mockUCenterResponse($fakeResponse)
-    {
-         // Create a mock and queue two responses.
-        $mock = new MockHandler([
-            new Response(200, [], json_encode($fakeResponse)),
-        ]);
-
-        $handler = HandlerStack::create($mock);
-        $client = new Client(['handler' => $handler]);
-        app()->instance(Client::class, $client);
     }
 
 }
